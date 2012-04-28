9b88beff021abdc1ffaf090b66199dfd738b56cc
Micha? Parko?a <michal.parkola@gmail.com>
2012-04-28 18:03:04 +0200

    Added a template-based details view for log entries.

--- unilog_core/views.py
+++ unilog_core/views.py
@@ -1,10 +1,14 @@
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from unilog_core.models import Tag, LogEntry
-from django.http import HttpResponse
+from django.http import Http404
 
 def index(request):
     entries = LogEntry.objects.all().order_by('date')
     return render_to_response('unilog_core/index.html', {'entries': entries})
 
 def detail(request, entry_id):
-    return HttpResponse("Hellow Detail View of entry: %s!" % str(entry_id))
+    try:
+        entry = LogEntry.objects.get(pk=entry_id)
+    except LogEntry.DoesNotExist:
+        raise Http404
+    return render_to_response("unilog_core/details.html", {'entry':entry})
