9fc3e8548a4ce5c1d1242b583c6bec6b2e98020c
Micha? Parko?a <michal.parkola@gmail.com>
2012-04-28 18:08:27 +0200

    Changed details view to use get_object_or_404 shortcut.

--- unilog_core/views.py
+++ unilog_core/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from unilog_core.models import Tag, LogEntry
-from django.http import Http404
 
 def index(request):
     entries = LogEntry.objects.all().order_by('date')
     return render_to_response('unilog_core/index.html', {'entries': entries})
 
 def detail(request, entry_id):
-    try:
-        entry = LogEntry.objects.get(pk=entry_id)
-    except LogEntry.DoesNotExist:
-        raise Http404
+    entry = get_object_or_404(LogEntry, pk=entry_id)
     return render_to_response("unilog_core/details.html", {'entry':entry})
