c4ddf0dae725e8af1b45ee7d741988d21b0db7e2
Micha? Parko?a <michal.parkola@gmail.com>
2012-04-29 17:07:08 +0200

    Switched to generic views for index and details.

--- unilog_core/urls.py
+++ unilog_core/urls.py
@@ -1,7 +1,28 @@
 from django.conf.urls import patterns, include, url
 
+# urlpatterns = patterns(
+#    'unilog_core.views',
+#    url(r'^$','index'),
+#    url(r'^(?P<entry_id>\d+)/$', 'detail'),
+#)
+
+from django.views.generic import ListView, DetailView
+from unilog_core.models import LogEntry
+
 urlpatterns = patterns(
-    'unilog_core.views',
-    url(r'^$','index'),
-    url(r'^(?P<entry_id>\d+)/$', 'detail'),
+    '',
+    url(r'^$',
+        ListView.as_view(
+            queryset = LogEntry.objects.order_by('-date'),
+            context_object_name='entries',
+            template_name='unilog_core/index.html'
+        )
+    ),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=LogEntry,
+            context_object_name='entry',
+            template_name='unilog_core/details.html'
+        )
+    )
 )
--- unilog_core/views.py
+++ unilog_core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from unilog_core.models import Tag, LogEntry
 
-def index(request):
-    entries = LogEntry.objects.all().order_by('date')
-    return render_to_response('unilog_core/index.html', {'entries': entries})
+#def index(request):
+#    entries = LogEntry.objects.all().order_by('-date')
+#    return render_to_response('unilog_core/index.html', {'entries': entries})
 
-def detail(request, entry_id):
-    entry = get_object_or_404(LogEntry, pk=entry_id)
-    return render_to_response("unilog_core/details.html", {'entry':entry})
+#def detail(request, entry_id):
+#    entry = get_object_or_404(LogEntry, pk=entry_id)
+#    return render_to_response("unilog_core/details.html", {'entry':entry})
