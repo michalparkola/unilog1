c425604271c49c7cb2d881e2a34d1318b1515f45
Micha? Parko?a <michal.parkola@gmail.com>
2012-09-06 01:48:24 +0200

    Installed South for database migrations

--- unilog/settings.py
+++ unilog/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'unilog_core',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
