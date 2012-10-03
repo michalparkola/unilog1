d2fe2ceead61400a4916b568a6e851df227dc412
Micha? Parko?a <michal.parkola@gmail.com>
2012-10-03 15:33:40 +0200

    Added user field to LogEntry model + migration.

--- unilog_core/models.py
+++ unilog_core/models.py
@@ -9,6 +9,8 @@ class LogEntry(models.Model):
     date = models.DateTimeField('Date and time of log entry')
     tags = models.ManyToManyField(Tag)
     text = models.TextField()
+    user = models.CharField(max_length=50)
+    
     def __unicode__(self):
         return self.text
     
