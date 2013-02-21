e126a8161ec942a8fb74730b107970ba3a86da7d
Micha? Parko?a <udreka@gmail.com>
2013-02-21 05:05:31 +0100

    Automatically set user for LogEntries based on who is logged in.

--- unilog_core/admin.py
+++ unilog_core/admin.py
@@ -1,5 +1,14 @@
 from unilog_core.models import Tag, LogEntry
 from django.contrib import admin
 
+class LogEntryAdmin(admin.ModelAdmin):
+    exclude = ('user',)
+    list_display = ('date','user','text')
+    
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.user = request.user
+        obj.save()
+
 admin.site.register(Tag)
-admin.site.register(LogEntry)
+admin.site.register(LogEntry, LogEntryAdmin)
