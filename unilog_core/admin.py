b683df00a17393abec40f619a4dd9399c54cb232
Micha? Parko?a <udreka@gmail.com>
2013-02-21 06:35:00 +0100

    Restrict admin access only to LogEntries that you created; hide entries by other users.

--- unilog_core/admin.py
+++ unilog_core/admin.py
@@ -10,5 +10,18 @@ class LogEntryAdmin(admin.ModelAdmin):
             obj.user = request.user
         obj.save()
 
+    def queryset(self, request):
+        if request.user.is_superuser:
+                return LogEntry.objects.all()
+        return LogEntry.objects.filter(user=request.user)
+        
+    def has_change_permission(self, request, obj=None):
+        has_class_permission = super(LogEntryAdmin, self).has_change_permission(request, obj)
+        if not has_class_permission:
+            return False
+        if obj is not None and not request.user.is_superuser and request.user.id != obj.user.id:
+            return False
+        return True
+
 admin.site.register(Tag)
 admin.site.register(LogEntry, LogEntryAdmin)
